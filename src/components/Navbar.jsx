50a9b26f00518b4003eb14786651b9e883aa0a4d
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-01-22 23:17:10 -0800

    made logo bigger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img onClick={() => navigate('/')} src={Logo} alt="Logo" style={{width: '30px'}} />
+        <img onClick={() => navigate('/')} src={Logo} alt="Logo" style={{width: '50px'}} />
       </div>
 
       {/* menu */}
